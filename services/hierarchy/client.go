4d8d38f23238e8dd8d139f71e289258492d963d1
Fredrik Lönnblad <f.lonnblad@gmail.com>
2018-07-28 13:22:07 +0200

    Added godoc for the Hierarchy service

--- services/hierarchy/client.go
+++ services/hierarchy/client.go
@@ -10,6 +10,9 @@ import (
 	"github.com/SKF/go-enlight-sdk/services/hierarchy/grpcapi"
 )
 
+// HierarchyClient provides the API operation methods for making
+// requests to Enlight Hierarchy Management Service. See this
+// package's package overview docs for details on the service.
 type HierarchyClient interface {
 	Dial(host, port string, opts ...grpc.DialOption) error
 	Close()
@@ -26,16 +29,19 @@ type HierarchyClient interface {
 	GetParentNode(nodeID string) (grpcapi.Node, error)
 }
 
-type client struct {
+// Client implements the HierarchyClient and holds the connection.
+type Client struct {
 	conn *grpc.ClientConn
 	api  grpcapi.HierarchyClient
 }
 
+// CreateClient creates an instance of the HierarchyClient.
 func CreateClient() HierarchyClient {
-	return &client{}
+	return &Client{}
 }
 
-func (c *client) Dial(host, port string, opts ...grpc.DialOption) (err error) {
+// Dial creates a client connection to the given host.
+func (c *Client) Dial(host, port string, opts ...grpc.DialOption) (err error) {
 	conn, err := grpc.Dial(host+":"+port, opts...)
 	if err != nil {
 		return
@@ -46,11 +52,13 @@ func (c *client) Dial(host, port string, opts ...grpc.DialOption) (err error) {
 	return
 }
 
-func (c *client) Close() {
+// Close tears down the ClientConn and all underlying connections.
+func (c *Client) Close() {
 	c.conn.Close()
 }
 
-func (c *client) DeepPing() error {
+// DeepPing pings the service to see if it is alive.
+func (c *Client) DeepPing() error {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
 
--- services/hierarchy/functions.go
+++ services/hierarchy/functions.go
@@ -10,14 +10,17 @@ import (
 	"github.com/SKF/go-enlight-sdk/services/hierarchy/grpcapi"
 )
 
-func (c *client) SaveNode(request grpcapi.SaveNodeInput) (string, error) {
+// SaveNode will add the node if it this not exist and otherwise
+// create it.
+func (c *Client) SaveNode(request grpcapi.SaveNodeInput) (string, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
 	defer cancel()
 	resp, err := c.api.SaveNode(ctx, &request)
 	return resp.GetValue(), err
 }
 
-func (c *client) GetNode(uuid string) (node grpcapi.Node, err error) {
+// GetNode takes an id of a node and returns the node.
+func (c *Client) GetNode(uuid string) (node grpcapi.Node, err error) {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
 	defer cancel()
 	resp, err := c.api.GetNode(ctx, &grpcapi.PrimitiveString{Value: uuid})
@@ -27,7 +30,9 @@ func (c *client) GetNode(uuid string) (node grpcapi.Node, err error) {
 	return
 }
 
-func (c *client) GetNodes(parentID string) (nodes []grpcapi.Node, err error) {
+// GetNodes will get all child nodes for the node id it takes as
+// an argument.
+func (c *Client) GetNodes(parentID string) (nodes []grpcapi.Node, err error) {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
 	defer cancel()
 	resp, err := c.api.GetNodes(ctx, &grpcapi.PrimitiveString{Value: parentID})
@@ -41,7 +46,9 @@ func (c *client) GetNodes(parentID string) (nodes []grpcapi.Node, err error) {
 	return
 }
 
-func (c *client) GetChildNodes(parentID string) (nodes []grpcapi.Node, err error) {
+// GetChildNodes will get all child nodes for the node id it takes as
+// an argument.
+func (c *Client) GetChildNodes(parentID string) (nodes []grpcapi.Node, err error) {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
 	defer cancel()
 	resp, err := c.api.GetChildNodes(ctx, &grpcapi.PrimitiveString{Value: parentID})
@@ -55,14 +62,18 @@ func (c *client) GetChildNodes(parentID string) (nodes []grpcapi.Node, err error
 	return
 }
 
-func (c *client) DeleteNode(request grpcapi.DeleteNodeInput) error {
+// DeleteNode will remove the node of the node id it takes as
+// an argument.
+func (c *Client) DeleteNode(request grpcapi.DeleteNodeInput) error {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
 	defer cancel()
 	_, err := c.api.DeleteNode(ctx, &request)
 	return err
 }
 
-func (c *client) GetParentNode(nodeID string) (node grpcapi.Node, err error) {
+// GetParentNode will return the parent of the node id it takes as
+// an argument.
+func (c *Client) GetParentNode(nodeID string) (node grpcapi.Node, err error) {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
 	defer cancel()
 	resp, err := c.api.GetParentNode(ctx, &grpcapi.PrimitiveString{Value: nodeID})
@@ -72,7 +83,9 @@ func (c *client) GetParentNode(nodeID string) (node grpcapi.Node, err error) {
 	return
 }
 
-func (c *client) GetAncestors(nodeID string) (nodes []grpcapi.AncestorNode, err error) {
+// GetAncestors will return all ancestors to the top of the node id
+// it takes as an argument.
+func (c *Client) GetAncestors(nodeID string) (nodes []grpcapi.AncestorNode, err error) {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
 	defer cancel()
 	resp, err := c.api.GetAncestors(ctx, &grpcapi.GetAncestorsInput{NodeId: nodeID})
@@ -86,7 +99,9 @@ func (c *client) GetAncestors(nodeID string) (nodes []grpcapi.AncestorNode, err 
 	return
 }
 
-func (c *client) GetEvents(since int, limit *int32) (events []eventsource.Record, err error) {
+// GetEvents will return all events that has occured in the Hierarchy
+// Management Service.
+func (c *Client) GetEvents(since int, limit *int32) (events []eventsource.Record, err error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 60*time.Second)
 	defer cancel()
 
