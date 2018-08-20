f30b3814ad0b5f41819817e4d8e60c38be370746
sjosidan <sjosidan@gmail.com>
2018-08-20 13:42:41 +0200

    added compliance-summary to reports service (#47)
    * added compliance-summary
    
    * fix indent

--- services/reports/mock/mock.go
+++ services/reports/mock/mock.go
@@ -72,3 +72,12 @@ func (mock *client) GetReportsWithContext(ctx context.Context, input api.GetRepo
 	args := mock.Called(ctx, input)
 	return args.Get(0).(*api.GetReportsOutput), args.Error(1)
 }
+
+func (mock *client) GetComplianceSummary(input api.GetComplianceSummaryInput) (output *api.GetComplianceSummaryOutput, err error) {
+	args := mock.Called(input)
+	return args.Get(0).(*api.GetComplianceSummaryOutput), args.Error(1)
+}
+func (mock *client) GetComplianceSummaryWithContext(ctx context.Context, input api.GetComplianceSummaryInput) (output *api.GetComplianceSummaryOutput, err error) {
+	args := mock.Called(ctx, input)
+	return args.Get(0).(*api.GetComplianceSummaryOutput), args.Error(1)
+}
