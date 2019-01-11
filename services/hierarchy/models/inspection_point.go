9bec0d1cf4b090c76f6f45d97c15036785b0af31
Ulf Karlsson <ulf.a.karlsson@skf.com>
2019-01-11 11:03:36 +0100

    Visualization type and min/max value

--- services/hierarchy/models/inspection_point.go
+++ services/hierarchy/models/inspection_point.go
@@ -3,6 +3,29 @@ package models
 type InspectionPoint struct {
 	ValueType   ValueType `json:"valueType"`
 	NumericUnit string    `json:"unit"`
+	Answers     Answers   `json:"answers"`
+
+	VisualizationType     VisualizationType `json:"visualizationType"`
+	VisualizationMinValue string            `json:"visualizationMinValue"`
+	VisualizationMaxValue string            `json:"visualizationMaxValue"`
 }
 
 type ValueType string
+
+const (
+	ValueTypeNumeric      ValueType = "numeric"
+	ValueTypeSingleChoice ValueType = "single_choice"
+	ValueTypeMultiChoice  ValueType = "multi_choice"
+)
+
+type Answers []string
+
+// VisualizationType - defines visualization type when value type is numeric
+type VisualizationType string
+
+// Constants for VisualizationType
+const (
+	VisualizationTypeNone          VisualizationType = "visualization_none"
+	VisualizationTypeCircularGauge VisualizationType = "visualization_circular_gauge"
+	VisualizationTypeLevelGauge    VisualizationType = "visualization_level_gauge"
+)
