a43d46ce8e935aad07e843a3ea5b783541cba8b3
Kasper Ingelvide <kasper.ingelvide@skf.com>
2023-03-27 16:36:15 +0200

    Refactoring

--- v2/services/authorize/client_retry_test.go
+++ v2/services/authorize/client_retry_test.go
@@ -34,6 +34,122 @@ var (
 
 const DistantFuture = 10 * 365 * 24 * time.Hour
 
+func setup(t *testing.T, authorizeServer dummyAuthorizeServer) (credentialsmanager.DataStore, server) {
+	privateKey, err := parseRSAKey()
+	require.NoError(t, err)
+
+	caCertPEM, err := generateCA(privateKey)
+	require.NoError(t, err)
+
+	serverDataStore, err := generateDatastore(ca, privateKey, caCertPEM, DistantFuture)
+	require.NoError(t, err)
+
+	clientDataStore, err := generateDatastore(ca, privateKey, caCertPEM, DistantFuture)
+	require.NoError(t, err)
+
+	tlsCredentials, err := loadTLSCredentials(serverDataStore)
+	require.NoError(t, err)
+
+	srv := newServer(tlsCredentials, authorizeServer)
+	err = srv.Start()
+	require.NoError(t, err)
+
+	return clientDataStore, srv
+}
+
+func TestDeadline(t *testing.T) {
+	clientDataStore, srv := setup(t, dummyAuthorizeServer{})
+
+	c := authorize.CreateClient()
+	c.SetRequestTimeout(0)
+
+	childCtx, cancel := context.WithTimeout(context.Background(), 100*time.Millisecond)
+	defer cancel()
+	err := c.DialUsingCredentialsManager(childCtx, &mockCredentialsFetcher{ds: clientDataStore}, "localhost", "10000", "")
+	require.NoError(t, err)
+
+	srv.Shutdown()
+
+	// Perform a call without specifying any deadline. As the server has been shut down this
+	// would normally result in the call blocking forever waiting for the client to reconnect.
+	// Assert: The call is intercepted and the request timeout is injected as a deadline.
+
+	_, err = c.GetResourceWithContext(context.Background(), "", "")
+
+	require.EqualError(t, err, "rpc error: code = DeadlineExceeded desc = context deadline exceeded",
+		"Caller omits deadline and the default request timeout is used")
+}
+
+func TestReconnect(t *testing.T) {
+	clientDataStore, srv := setup(t, dummyAuthorizeServer{})
+
+	c := authorize.CreateClient()
+
+	err := c.DialUsingCredentialsManager(context.Background(), &mockCredentialsFetcher{ds: clientDataStore}, "localhost", "10000", "")
+	require.NoError(t, err)
+
+	err = srv.Restart()
+	require.NoError(t, err)
+	defer srv.Shutdown()
+
+	_, err = c.GetResourceWithContext(context.Background(), "", "")
+
+	require.NoError(t, err)
+}
+
+func TestRetryPolicy(t *testing.T) {
+	clientDataStore, srv := setup(t, dummyAuthorizeServer{
+		failuresRemaining: 4,
+	})
+	defer srv.Shutdown()
+
+	c := authorize.CreateClient()
+
+	err := c.DialUsingCredentialsManager(context.Background(), &mockCredentialsFetcher{ds: clientDataStore}, "localhost", "10000", "")
+	require.NoError(t, err)
+
+	_, err = c.GetResourceWithContext(context.Background(), "", "")
+	require.NoError(t, err)
+}
+
+func TestClientHandshake_CertificateAboutToExpire(t *testing.T) {
+	privateKey, err := parseRSAKey()
+	require.NoError(t, err)
+
+	caCertPEM, err := generateCA(privateKey)
+	require.NoError(t, err)
+
+	ds, err := generateDatastore(ca, privateKey, caCertPEM, authorize.CertificateGracePeriod-time.Second)
+	require.NoError(t, err)
+
+	cf := &mockCredentialsFetcher{ds: ds}
+
+	ctx := context.Background()
+	tls, err := authorize.NewAutoRefreshingTransportCredentials(ctx, cf, "secret", "localhost")
+	require.NoError(t, err)
+
+	require.Equal(t, 1, cf.callCount, "Certificates are loaded once during initialization")
+
+	server, client := net.Pipe()
+	// Close pipe to avoid client getting stuck in an infinite reconnect loop
+	err = server.Close()
+	require.NoError(t, err)
+
+	// Swap out certificates for fresh ones
+	cf.ds, err = generateDatastore(ca, privateKey, caCertPEM, DistantFuture)
+	require.NoError(t, err)
+
+	_, _, err = tls.ClientHandshake(ctx, "", client)
+	require.Error(t, err, "io: read/write on closed pipe")
+
+	require.Equal(t, 2, cf.callCount, "Certificates are about to expiry and should be reloaded")
+
+	_, _, err = tls.ClientHandshake(ctx, "", client)
+	require.Error(t, err, "io: read/write on closed pipe")
+
+	require.Equal(t, 2, cf.callCount, "Cached certificates are still valid and should not be reloaded")
+}
+
 var ca = &x509.Certificate{
 	SerialNumber:          big.NewInt(2019),
 	Subject:               pkix.Name{},
@@ -166,184 +282,53 @@ func parseRSAKey() (*rsa.PrivateKey, error) {
 }
 
 type server struct {
-	signal chan int
+	signal          chan struct{}
+	tlsCredentials  credentials.TransportCredentials
+	authorizeServer dummyAuthorizeServer
 }
 
-func launchServer(tlsCredentials credentials.TransportCredentials, authorizeServer dummyAuthorizeServer) (server, error) {
+func newServer(tlsCredentials credentials.TransportCredentials, authorizeServer dummyAuthorizeServer) server {
 	server := server{
-		signal: make(chan int),
+		signal:          make(chan struct{}),
+		tlsCredentials:  tlsCredentials,
+		authorizeServer: authorizeServer,
 	}
 
+	return server
+}
+
+func (s *server) Start() error {
 	var serverOpts []grpc.ServerOption
-	serverOpts = append(serverOpts, grpc.Creds(tlsCredentials))
+	serverOpts = append(serverOpts, grpc.Creds(s.tlsCredentials))
 
 	grpcServer := grpc.NewServer(serverOpts...)
-	authorizeproto.RegisterAuthorizeServer(grpcServer, &authorizeServer)
+	authorizeproto.RegisterAuthorizeServer(grpcServer, &s.authorizeServer)
 
 	lis, err := net.Listen("tcp", "localhost:10000")
 	if err != nil {
-		return server, err
+		return err
 	}
 
 	go func() {
 		//nolint:errcheck
 		go grpcServer.Serve(lis)
 
-		<-server.signal
+		<-s.signal
 
 		grpcServer.Stop()
 		_ = lis.Close()
-		server.signal <- 1
+		s.signal <- struct{}{}
 	}()
 
-	return server, nil
+	return nil
 }
 
 func (s *server) Shutdown() {
-	s.signal <- 1
+	s.signal <- struct{}{}
 	<-s.signal
-	close(s.signal)
 }
 
-func TestDeadline(t *testing.T) {
-	privateKey, err := parseRSAKey()
-	require.NoError(t, err)
-
-	caCertPEM, err := generateCA(privateKey)
-	require.NoError(t, err)
-
-	serverDataStore, err := generateDatastore(ca, privateKey, caCertPEM, DistantFuture)
-	require.NoError(t, err)
-
-	clientDataStore, err := generateDatastore(ca, privateKey, caCertPEM, DistantFuture)
-	require.NoError(t, err)
-
-	tlsCredentials, err := loadTLSCredentials(serverDataStore)
-	require.NoError(t, err)
-
-	server, err := launchServer(tlsCredentials, dummyAuthorizeServer{})
-	require.NoError(t, err)
-
-	c := authorize.CreateClient()
-	c.SetRequestTimeout(0)
-
-	childCtx, cancel := context.WithTimeout(context.Background(), 100*time.Millisecond)
-	defer cancel()
-	err = c.DialUsingCredentialsManager(childCtx, &mockCredentialsFetcher{ds: clientDataStore}, "localhost", "10000", "")
-	require.NoError(t, err)
-
-	server.Shutdown()
-
-	// Perform a call without specifying any deadline. As the server has been shut down this
-	// would normally result in the call blocking forever waiting for the client to reconnect.
-	// Assert: The call is intercepted and the request timeout is injected as a deadline.
-
-	_, err = c.GetResourceWithContext(context.Background(), "", "")
-
-	require.EqualError(t, err, "rpc error: code = DeadlineExceeded desc = context deadline exceeded",
-		"Caller omits deadline and the default request timeout is used")
-}
-
-func TestReconnect(t *testing.T) {
-	privateKey, err := parseRSAKey()
-	require.NoError(t, err)
-
-	caCertPEM, err := generateCA(privateKey)
-	require.NoError(t, err)
-
-	serverDataStore, err := generateDatastore(ca, privateKey, caCertPEM, DistantFuture)
-	require.NoError(t, err)
-
-	clientDataStore, err := generateDatastore(ca, privateKey, caCertPEM, DistantFuture)
-	require.NoError(t, err)
-
-	tlsCredentials, err := loadTLSCredentials(serverDataStore)
-	require.NoError(t, err)
-
-	server, err := launchServer(tlsCredentials, dummyAuthorizeServer{})
-	require.NoError(t, err)
-
-	c := authorize.CreateClient()
-
-	err = c.DialUsingCredentialsManager(context.Background(), &mockCredentialsFetcher{ds: clientDataStore}, "localhost", "10000", "")
-	require.NoError(t, err)
-
-	server.Shutdown()
-
-	server, err = launchServer(tlsCredentials, dummyAuthorizeServer{})
-	require.NoError(t, err)
-	defer server.Shutdown()
-
-	_, err = c.GetResourceWithContext(context.Background(), "", "")
-
-	require.NoError(t, err)
-}
-
-func TestRetryPolicy(t *testing.T) {
-	privateKey, err := parseRSAKey()
-	require.NoError(t, err)
-
-	caCertPEM, err := generateCA(privateKey)
-	require.NoError(t, err)
-
-	serverDataStore, err := generateDatastore(ca, privateKey, caCertPEM, DistantFuture)
-	require.NoError(t, err)
-
-	clientDataStore, err := generateDatastore(ca, privateKey, caCertPEM, DistantFuture)
-	require.NoError(t, err)
-
-	tlsCredentials, err := loadTLSCredentials(serverDataStore)
-	require.NoError(t, err)
-
-	server, err := launchServer(tlsCredentials, dummyAuthorizeServer{
-		failuresRemaining: 4,
-	})
-	require.NoError(t, err)
-	defer server.Shutdown()
-
-	c := authorize.CreateClient()
-
-	err = c.DialUsingCredentialsManager(context.Background(), &mockCredentialsFetcher{ds: clientDataStore}, "localhost", "10000", "")
-	require.NoError(t, err)
-
-	_, err = c.GetResourceWithContext(context.Background(), "", "")
-	require.NoError(t, err)
-}
-
-func TestClientHandshake_CertificateAboutToExpire(t *testing.T) {
-	privateKey, err := parseRSAKey()
-	require.NoError(t, err)
-
-	caCertPEM, err := generateCA(privateKey)
-	require.NoError(t, err)
-
-	ds, err := generateDatastore(ca, privateKey, caCertPEM, authorize.CertificateGracePeriod-time.Second)
-	require.NoError(t, err)
-
-	cf := &mockCredentialsFetcher{ds: ds}
-
-	ctx := context.Background()
-	tls, err := authorize.NewAutoRefreshingTransportCredentials(ctx, cf, "secret", "localhost")
-	require.NoError(t, err)
-
-	require.Equal(t, 1, cf.callCount, "Certificates are loaded once during initialization")
-
-	server, client := net.Pipe()
-	// Close pipe to avoid client getting stuck in an infinite reconnect loop
-	err = server.Close()
-	require.NoError(t, err)
-
-	// Swap out certificates for fresh ones
-	cf.ds, err = generateDatastore(ca, privateKey, caCertPEM, DistantFuture)
-	require.NoError(t, err)
-
-	_, _, err = tls.ClientHandshake(ctx, "", client)
-	require.Error(t, err, "io: read/write on closed pipe")
-
-	require.Equal(t, 2, cf.callCount, "Certificates are about to expiry and should be reloaded")
-
-	_, _, err = tls.ClientHandshake(ctx, "", client)
-	require.Error(t, err, "io: read/write on closed pipe")
-
-	require.Equal(t, 2, cf.callCount, "Cached certificates are still valid and should not be reloaded")
+func (s *server) Restart() error {
+	s.Shutdown()
+	return s.Start()
 }
